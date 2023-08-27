314c7f32415677ebcd92c1ae822921855edc06c6
CrazyBoy49z <yura.finiv@gmail.com>
2023-08-27 23:27:09 +0300

    WIP

--- app/Livewire/Docs/Loading.php
+++ app/Livewire/Docs/Loading.php
@@ -2,11 +2,12 @@
 
 namespace App\Livewire\Docs;
 
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class Loading extends Component
 {
-    public function render()
+    public function render(): View
     {
         return view('livewire.docs.loading')
             ->layoutData([
